bd87079df3da73abd657d728330e1475c4f8a469
ppaules9] <pavlypaules9@gmail.com>
2025-11-06 13:06:42 +0200

    Initial Commit

---
+++ Agilo For Her/Agilo_For_HerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Agilo_For_HerApp.swift
+//  Agilo For Her
+//
+//  Created by Pavly Paules on 06/11/2025.
+//
+
+import SwiftUI
+
+@main
+struct Agilo_For_HerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
